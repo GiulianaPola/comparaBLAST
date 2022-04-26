a26c9328d0d5d395c9db84f43ceb0c62eab8f081
Giuliana Pola <92754890+GiulianaPola@users.noreply.github.com>
2022-04-26 01:11:27 +0000

    (1.1.0) Incorporating the stitle column of databases A and B:
    ID seq columns for A, evalue for A, stitle for A, ID seq for B, e-value, stitle for B, ID seq for A/B, e-value in A, stitle for A, evalue in B, stitle for B, decision (A or B)

---
+++ README.md
@@ -0,0 +1,26 @@
+# comparaBLAST
+Program for comparing sequence BLAST results against two databases
+
+## Input file (-a or -b)
+The results are in tabular format files and were generated with BLAST used e-value filter and the parameter -max_target_seqs 1 (only the best hit for each sequence). 
+Columns:
+qseqid,sseqid,length,pident,bitscore,score,evalue,stitle
+
+## Versions
+
+### 10/10/2021 (1.0.0)
+Redundancies were reduced by eliminating results with a lower e-value or bitscore since a sequence may encounter numerous alignment blocks. It was established if the affirmative sequences are present exclusively in databases A and B, or in both. It was determined which bank had the most significant result for the set of positive sequences in both databases based on the e-value (the more negative the e-value, the better). Both banks A and B's positive end sequences were listed. The positive end sequences for banks A, B, or both were saved to output file:
+
+ID seq columns for A, evalue for A, ID seq for B, e-value, ID seq for A/B, e-value in A, evalue in B, decision (A or B)
+
+### 18/10/2021 (1.0.1)
+Inserting the Python shebang line: 
+
+!/usr/bin/python
+
+Incorporating help with -h or -help or no parameter 
+
+### 25/04/2022 (1.1.0)
+Incorporating the stitle column of databases A and B:
+
+ID seq columns for A, evalue for A, stitle for A, ID seq for B, e-value, stitle for B, ID seq for A/B, e-value in A, stitle for A, evalue in B, stitle for B, decision (A or B)
--- comparaBLAST.py
+++ comparaBLAST.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 
-#python comparaBLAST.py -a Fh_com_filtro_30_bats.txt -b Fh_com_filtro_30_vir.txt -o Fh_comparado.tab
-#python comparaBLAST.py -a La_com_filtro_30_bats.txt -b La_com_filtro_30_vir.txt -o La_comparado.tab
-#python comparaBLAST.py -a Nm_com_filtro_30_bats.txt -b Nm_com_filtro_30_vir.txt -o Nm_comparado.tab
-
 import argparse
 import csv
 
-ajuda = 'ComparaBLAST - comparison of sequence BLAST results against two databases\n'
-ajuda=ajuda+'(c) 2021. Arthur Gruber, Giuliana Pola & Liliane S. Oliveira\n'
-ajuda = ajuda + 'Usage: comparaBLAST.py -a <tabular file against database A> -b <tabular file against database B> -o <output file> \n'
-ajuda = ajuda + '\nMandatory parameters:\n'
-ajuda = ajuda + '-a\tTabular file against database A\n'
-ajuda = ajuda + '-b\tTabular file against database B\n'
-ajuda = ajuda + '\nOptional parameters:\n'
-ajuda = ajuda + '-o\tOutput file (standard:"comparado.tab")\n'
+version='1.1.0'
+
+menu = 'ComparaBLAST v{} - comparison of sequence BLAST results against two databases\n'.format(version)
+menu = menu + '(c) 2021. Arthur Gruber, Giuliana Pola & Liliane S. Oliveira\n'
+menu = menu + 'Usage: comparaBLAST.py -a <tabular file against database A> -b <tabular file against database B> -o <output file> \n'
+menu = menu + '\nMandatory parameters:\n'
+menu = menu + '-a\tTabular file against database A\n'
+menu = menu + '-b\tTabular file against database B\n'
+menu = menu + '\nOptional parameters:\n'
+menu = menu + '-o\tOutput file (default:"comparado.tab")\n'
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-a')
@@ -22,91 +20,157 @@ parser.add_argument('-b')
 parser.add_argument('-o')
 parser.add_argument('-h', '--help', action='store_true')
 args = parser.parse_args()
+processed=[]
+
+def getfilename(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+def order(filename):
+    try:
+      file=open(filename, 'r')
+    except:
+      print("File '{}' cannot be opened!".getfilename(filename))
+      quit()
+    else:
+      seqs = dict()
+      for row in file.readlines():
+          colunas = row.split('\t')
+          qseqid = colunas[0]
+          bitscore = float(colunas[4])
+          evalue = float(colunas[6])
+          if len(colunas)==8:
+            stitle=colunas[7]
+          if qseqid in seqs.keys():
+              item = seqs[qseqid]
+              if evalue < item[1]:
+                  #print(evalue,'<',item[1])
+                  if len(colunas)==8:
+                    seqs[qseqid] = [bitscore, evalue,stitle]
+                  else:
+                    seqs[qseqid] = [bitscore, evalue]
+              elif evalue == item[1] and bitscore > item[0]:
+                  #print(evalue,'==',item[1],' and ',bitscore,'>',item[0])
+                  if len(colunas)==8:
+                    seqs[qseqid] = [bitscore, evalue,stitle]
+                  else:
+                    seqs[qseqid] = [bitscore, evalue]
+          if not qseqid in seqs.keys():
+              if len(colunas)==8:
+                seqs[qseqid] = [bitscore, evalue,stitle]
+              else:
+                seqs[qseqid] = [bitscore, evalue]
+      return seqs
 
 
-def ordenar(arquivo):
-    seqs = dict()
-    for linha in arquivo.readlines():
-        colunas = linha.split('\t')
-        qseqid = colunas[0]
-        bitscore = float(colunas[4])
-        evalue = float(colunas[6])
-        if qseqid in seqs.keys():
-            item = seqs[qseqid]
-            if evalue < item[1]:
-              #print(evalue,'<',item[1])
-              seqs[qseqid] = [bitscore,evalue]
-            elif evalue == item[1] and bitscore > item[0]:
-                #print(evalue,'==',item[1],' and ',bitscore,'>',item[0])
-                seqs[qseqid] = [bitscore,evalue]
-        if not qseqid in seqs.keys():
-            seqs[qseqid] = [bitscore,evalue]
-    return seqs
-
-
-def comparar(A, B):
+def compare(A, B):
     AB = dict()
     seqs = list(A.keys()) + list(B.keys())
     for qseqid in seqs:
         if qseqid in A.keys() and qseqid in B.keys():
             x = A[qseqid]
             y = B[qseqid]
+            if ncols==2:
+              xy=[x[1],y[1]]
+            elif ncols==3:
+              if len(x)==3 and len(y)==3:
+                xy=[x[1],x[2],y[1],y[2]]
+              elif len(x)==2 and len(y)==3:
+                xy=[x[1],'',y[1],y[2]]
+              elif len(x)==3 and len(y)==2:
+                xy=[x[1],x[2],y[1],'']
             if x[1] < y[1]:
-                #print(x[1],'<',y[1],' -> A')
-                AB[qseqid] = [x[1], y[1], 'A']
+#               print(x[1],'<',y[1],' -> A')
+                xy.append('A')
+                AB[qseqid] = xy
             elif x[1] > y[1]:
-                #print(x[1],'>',y[1],' -> B')
-                AB[qseqid] = [x[1], y[1],'B']
+#               print(x[1],'>',y[1],' -> B')
+                xy.append('B')
+                AB[qseqid] = xy
             elif x[1] == y[1] and x[0] > y[0]:
-                #print(x[1],'==',y[1],' and ',x[0],' > ',y[0],' -> A')
-                AB[qseqid] = [x[1], y[1],'A']
+#               print(x[1],'==',y[1],' and ',x[0],' > ',y[0],' -> A')
+                xy.append('A')
+                AB[qseqid] = xy
             elif x[1] == y[1] and y[0] > x[0]:
-                #print(x[1],'==',y[1],' and ',x[0],' < ',y[0],' -> B')
-                AB[qseqid] = [x[1], y[1],'B']
+#                print(x[1],'==',y[1],' and ',x[0],' < ',y[0],' -> B')
+                xy.append('B')
+                AB[qseqid] = xy
             elif x[1] == y[1] and x[0] == y[0]:
-                #print(x[1],'==',y[1],' and ',x[0],' == ',y[0],' -> AB')
-                AB[qseqid] = [x[1], y[1],'AB']
+#               print(x[1],'==',y[1],' and ',x[0],' == ',y[0],' -> AB')
+                xy.append('AB')
+                AB[qseqid] = xy
     return AB
 
-if args._get_args()==None:
-  print(ajuda)
+
+if args.a == None and args.b == None and args.help == False and args.o == None:
+    print(menu)
 elif args.help == True:
-    print(ajuda)
+    print(menu)
 elif args.a == None or args.b == None:
     print('ERROR: Missing input file\n')
-    print(ajuda)
+    print(menu)
 else:
-    A = ordenar(open(args.a, 'r'))
-    B = ordenar(open(args.b, 'r'))
-    AB = comparar(A, B)
+    A = order(args.a)
+    B = order(args.b)
+    ncols=max(len(list(A.values())[0]),len(list(B.values())[0]))
+    AB = compare(A, B)
     if args.o == None:
-      saida = open('comparado.tab', mode='w')
+      output = open('compared.tab', mode='w')
     else:
-      saida = open(args.o, mode='w')
-      escritor = csv.writer(saida,
-                          delimiter='\t',
-                          quotechar='"',
-                          quoting=csv.QUOTE_MINIMAL)
-      escritor.writerow([
-        'A_qseqid', 'A_evalue', 'B_qseqid', 'B_evalue', 'AB_qseqid',
-        'A_evalue', 'B_evalue', 'Escolha'
-    ])
-      for i in range(max(len(A), len(B), len(AB))):
-        linha = []
-        if i < len(A):
-            linha.append(list(A.keys())[i])
-            linha.append(list(A[linha[0]])[0])
-        else:
-            linha.extend(['', ''])
-        if i < len(B):
-            linha.append(list(B.keys())[i])
-            linha.append(list(B[linha[2]])[0])
-        else:
-            linha.extend(['', ''])
-        if i < len(AB):
-            linha.append(list(AB.keys())[i])
-            linha.extend(AB[linha[4]])
-        else:
-            linha.extend(['', '', '', ''])
-        escritor.writerow(linha)
-    saida.close()
+      output = open(args.o, mode='w')
+    writer = csv.writer(output,
+                          delimiter='\t')
+    if ncols==2:
+      writer.writerow([
+          'A_qseqid', 'A_evalue', 'B_qseqid', 'B_evalue', 'AB_qseqid',
+          'A_evalue', 'B_evalue', 'Escolha'
+      ])
+    elif ncols==3:
+      writer.writerow([
+          'A_qseqid', 'A_evalue', 'A_stitle', 'B_qseqid', 'B_evalue', 'B_stitle', 'AB_qseqid',
+          'A_evalue',  'A_stitle', 'B_evalue', 'A_stitle', 'Escolha'
+      ])
+    i=0
+    qseqids=sorted(list(AB.keys()))
+    qseqids.extend(sorted(list(A.keys())))
+    qseqids.extend(sorted(list(B.keys())))
+    for qseqid in qseqids:
+        row = []
+        if qseqid not in processed:
+         if qseqid in A.keys():
+           row.append(qseqid) #A_qseqid
+           row.append(list(A[qseqid])[1]) #A_evalue
+           if ncols==3:
+             if len(list(A[qseqid]))==3:
+               row.append(list(A[qseqid])[2]) #A_stitle
+             else:
+               row.append('')
+         else:
+           if ncols==3:
+             row.extend(['', '',''])
+           elif ncols==2:
+             row.extend(['', ''])
+         if qseqid in B.keys():
+           row.append(qseqid) #B_qseqid
+           row.append(list(B[qseqid])[1]) #B_evalue
+           if ncols==3:
+             if len(list(B[qseqid]))==3:
+               row.append(list(B[qseqid])[2]) #B_stitle
+             else:
+               row.append('')
+         else:
+           if ncols==3:
+             row.extend(['', '',''])
+           elif ncols==2:
+             row.extend(['', ''])
+         if qseqid in AB.keys():
+           row.append(qseqid) #AB_qseqid
+           row.extend(AB[qseqid]) #A_evalue, (A_stitle), B_evalue, (B_stitle), Escolha
+         else:
+           if ncols==3:
+             row.extend(['', '','','','',''])
+           elif ncols==2:
+             row.extend(['', '','',''])
+         writer.writerow(row)
+         processed.append(qseqid)
+    output.close()
